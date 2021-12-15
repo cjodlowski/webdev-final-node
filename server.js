3f6bfbdaafc24643da5517b43567c89a93610c2a
soph0002 <uldrylavergne.s@husky.neu.edu>
2021-12-14 20:40:00 -0500

    udate

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept");
+    "Origin, X-Requested-With, Content-Type, Accept, Credentials");
   res.header("Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
